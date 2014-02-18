3d3e098c787b53d7fc8fa83f048bc3ed6636c53f
ivanlesko <mail@ivanlesko.com>
2014-02-17 22:20:09 -0800

    Implemented the space theme and added 8-bit background music on start.

--- SpriteDemo/ZMBMyScene.h
+++ SpriteDemo/ZMBMyScene.h
@@ -8,11 +8,15 @@
 
 #import <SpriteKit/SpriteKit.h>
 
+@import AVFoundation;
+
 @interface ZMBMyScene : SKScene {
-    
+    AVAudioPlayer *backgroundAudioPlayer;
 }
 
-@property (nonatomic, strong) NSMutableArray *shipFrames;
+@property (nonatomic, strong) NSMutableArray *shipFramesCenter;
+@property (nonatomic, strong) NSMutableArray *shipFramesUp;
+@property (nonatomic, strong) NSMutableArray *shipFramesDown;
 
 - (void)setupShip;
 
--- SpriteDemo/ZMBMyScene.m
+++ SpriteDemo/ZMBMyScene.m
@@ -11,8 +11,8 @@
 @interface ZMBMyScene ()
 
 {
-    int _nextFlappy;
-    double _nextFlappySpawn;
+    int _nextAsteroid;
+    double _nextAsteroidSpawn;
     NSMutableArray *_shipLasers;
     int _nextShipLaser;
     int _lives;
@@ -21,9 +21,9 @@
 }
 
 @property (strong, nonatomic) SKSpriteNode *ship;
-@property (strong, nonatomic) NSMutableArray *flappyArray;
+@property (strong, nonatomic) NSMutableArray *asteroidArray;
 
-#define kNumFlappys 8
+#define kNumAsteroids 8
 #define kNumLasers 5
 
 typedef enum {
@@ -35,56 +35,109 @@ typedef enum {
 
 @implementation ZMBMyScene
 
+
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        _nextFlappy = 0;
+        _nextAsteroid = 0;
         
-        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
-        for (int i = 0; i < 2; i++) {
-            SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:@"starBackground"];
-            bg.anchorPoint = CGPointZero;
-            bg.position = CGPointMake(i * bg.size.width, 0);
-            bg.name = @"starBackground";
-            [self addChild:bg];
-        }
+        [self startBackgroundMusic];
+        
+        [self setupBoundsToScreen];
         
         [self setupShip];
         
-        self.ship.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.ship.size];
-        self.ship.physicsBody.dynamic = YES;
-        self.ship.physicsBody.affectedByGravity = NO;
-        self.ship.physicsBody.mass = 0.02;
+        [self setupAsteroids];
         
-        self.flappyArray = [[NSMutableArray alloc] initWithCapacity:kNumFlappys];
-        
-        for (int i = 0; i < kNumFlappys; i++) {
-            SKSpriteNode *flappy = [SKSpriteNode spriteNodeWithImageNamed:@"IJ"];
-            flappy.position = CGPointMake(700, 100);
-            flappy.hidden = YES;
-            [self.flappyArray addObject:flappy];
-            [self addChild:flappy];
-        }
-        
-#pragma mark - TBD - Setup the lasers
-        
-        _shipLasers = [[NSMutableArray alloc] initWithCapacity:kNumLasers];
-        for (int i = 0; i < kNumLasers; i++) {
-            SKSpriteNode *shipLaser = [SKSpriteNode spriteNodeWithImageNamed:@"laserbeam_blue"];
-            shipLaser.hidden = YES;
-            [_shipLasers addObject:shipLaser];
-            [self addChild:shipLaser];
-        }
-        
-        for (SKSpriteNode *laser in _shipLasers) {
-            laser.hidden = YES;
-        }
+        [self setupLasers];
         
         [self startTheGame];
         
     }
     return self;
+}
+
+- (void)setupBoundsToScreen {
+    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    for (int i = 0; i < 2; i++) {
+        SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:@"starBackground"];
+        bg.anchorPoint = CGPointZero;
+        bg.position = CGPointMake(i * bg.size.width, 0);
+        bg.name = @"starBackground";
+        [self addChild:bg];
+    }
+}
+
+- (void)setupShip {
+    self.shipFramesCenter = [NSMutableArray array];
+    self.shipFramesUp     = [NSMutableArray array];
+    self.shipFramesDown   = [NSMutableArray array];
+    
+    SKTextureAtlas *shipAnimatedCenterAtlas = [SKTextureAtlas atlasNamed:@"ship_Center"];
+    SKTextureAtlas *shipAnimatedDownAtlas   = [SKTextureAtlas atlasNamed:@"ship_Down"];
+    SKTextureAtlas *shipAnimatedUpAtlas     = [SKTextureAtlas atlasNamed:@"ship_Up"];
+    
+    // Add each frame of the ship animation to the ship frames array. (Once for each ship position).
+    
+    // Center frames
+    for (int i = 1; i < shipAnimatedCenterAtlas.textureNames.count + 1; i++) {
+        NSString *textName = [NSString stringWithFormat:@"shipCenter_%d.png", i];
+        SKTexture *texture = [shipAnimatedCenterAtlas textureNamed:textName];
+        [self.shipFramesCenter addObject:texture];
+    }
+    
+    // Down frames
+    for (int i = 1; i < shipAnimatedDownAtlas.textureNames.count + 1; i++) {
+        NSString *textName = [NSString stringWithFormat:@"shipDown_%d.png", i];
+        SKTexture *texture = [shipAnimatedDownAtlas textureNamed:textName];
+        [self.shipFramesDown addObject:texture];
+    }
+    
+    // Up frames
+    for (int i = 1; i < shipAnimatedUpAtlas.textureNames.count + 1; i++) {
+        NSString *textName = [NSString stringWithFormat:@"shipUp_%d.png", i];
+        SKTexture *texture = [shipAnimatedUpAtlas textureNamed:textName];
+        [self.shipFramesUp addObject:texture];
+    }
+    
+    SKTexture *texture = self.shipFramesCenter[0];
+    self.ship = [SKSpriteNode spriteNodeWithTexture:texture];
+    self.ship.position = CGPointMake(50, CGRectGetMidY(self.frame));
+    self.ship.size = CGSizeMake(self.ship.size.width / 2, self.ship.size.height / 2);
+    [self addChild:self.ship];
+    
+    // Animate the ship.
+    [self.ship runAction:[SKAction repeatActionForever:[SKAction animateWithTextures:self.shipFramesCenter
+                                                                        timePerFrame:0.1f
+                                                                              resize:NO
+                                                                             restore:YES]] withKey:@"animatingShipCenter"];
+}
+
+- (void)setupAsteroids {
+    self.asteroidArray = [[NSMutableArray alloc] initWithCapacity:kNumAsteroids];
+    
+    for (int i = 0; i < kNumAsteroids; i++) {
+        SKSpriteNode *asteroid = [SKSpriteNode spriteNodeWithImageNamed:@"asteroid_01"];
+        asteroid.position = CGPointMake(700, 100);
+        asteroid.hidden = YES;
+        [self.asteroidArray addObject:asteroid];
+        [self addChild:asteroid];
+    }
+}
+
+- (void)setupLasers {
+    _shipLasers = [[NSMutableArray alloc] initWithCapacity:kNumLasers];
+    for (int i = 0; i < kNumLasers; i++) {
+        SKSpriteNode *shipLaser = [SKSpriteNode spriteNodeWithImageNamed:@"laserbeam_blue"];
+        shipLaser.hidden = YES;
+        [_shipLasers addObject:shipLaser];
+        [self addChild:shipLaser];
+    }
+    
+    for (SKSpriteNode *laser in _shipLasers) {
+        laser.hidden = YES;
+    }
 }
 
 - (void)startTheGame
@@ -95,6 +148,28 @@ typedef enum {
     _gameOver = NO;
     
     self.ship.hidden = NO;
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    
+    CGPoint positionInScene = [touch locationInNode:self];
+    CGPoint previousPosition = [touch previousLocationInNode:self];
+    CGPoint translation = CGPointMake(positionInScene.x - previousPosition.x,
+                                      positionInScene.y - previousPosition.y);
+    
+    // Animate the ship up or down based on finger position.
+    [self moveShipWithTranslation:translation];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"touches ended");
+    [self.ship removeActionForKey:@"animatingShipUp"];
+    [self.ship removeActionForKey:@"animatingShipDown"];
+    [self.ship runAction:[SKAction repeatActionForever:[SKAction animateWithTextures:self.shipFramesCenter
+                                                                        timePerFrame:0.1f
+                                                                              resize:NO
+                                                                             restore:YES]] withKey:@"animatingShipCenter"];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -116,7 +191,6 @@ typedef enum {
         return;
     }
     
-    
     SKSpriteNode *shipLaser = [_shipLasers objectAtIndex:_nextShipLaser];
     _nextShipLaser++;
     if (_nextShipLaser >= _shipLasers.count) {
@@ -137,32 +211,6 @@ typedef enum {
     SKAction *moveLaserActionWithDone = [SKAction sequence:@[laserMoveAction,laserDoneAction]];
     
     [shipLaser runAction:moveLaserActionWithDone withKey:@"laserFired"];
-    
-}
-
-- (void)setupShip {
-    self.shipFrames = [NSMutableArray array];
-    
-    SKTextureAtlas *shipAnimatedAtlas = [SKTextureAtlas atlasNamed:@"ship_Center"];
-    
-    // Add each frame of the ship animation to the ship frames array.
-    for (int i = 1; i < shipAnimatedAtlas.textureNames.count + 1; i++) {
-        NSString *textName = [NSString stringWithFormat:@"shipCenter_%d.png", i];
-        SKTexture *texture = [shipAnimatedAtlas textureNamed:textName];
-        [self.shipFrames addObject:texture];
-    }
-    
-    SKTexture *texture = self.shipFrames[0];
-    self.ship = [SKSpriteNode spriteNodeWithTexture:texture];
-    self.ship.position = CGPointMake(50, CGRectGetMidY(self.frame));
-    self.ship.size = CGSizeMake(self.ship.size.width / 2, self.ship.size.height / 2);
-    [self addChild:self.ship];
-    
-    // Animate the ship.
-    [self.ship runAction:[SKAction repeatActionForever:[SKAction animateWithTextures:self.shipFrames
-                                                                        timePerFrame:0.1f
-                                                                              resize:NO
-                                                                             restore:YES]] withKey:@"animatingShip"];
 }
 
 -(void)update:(CFTimeInterval)currentTime {
@@ -180,63 +228,62 @@ typedef enum {
     
     double curTime = CACurrentMediaTime();
     
-    if (curTime > _nextFlappySpawn) {
+    if (curTime > _nextAsteroidSpawn) {
         
         float randSeconds = [self randomValueBetween:0.20f andValue:1.0f];
-        _nextFlappySpawn = randSeconds + curTime;
+        _nextAsteroidSpawn = randSeconds + curTime;
         
         float randY = [self randomValueBetween:0.0f andValue:self.frame.size.height];
         float randDuration = [self randomValueBetween:5.0f andValue:8.0f];
         
-        SKSpriteNode *flappy = self.flappyArray[_nextFlappy];
-        _nextFlappy++;
+        SKSpriteNode *asteroid = self.asteroidArray[_nextAsteroid];
+        _nextAsteroid++;
         
-        if (_nextFlappy >= self.flappyArray.count) {
-            _nextFlappy = 0;
+        if (_nextAsteroid >= self.asteroidArray.count) {
+            _nextAsteroid = 0;
         }
         
-        [flappy removeAllActions];
-        flappy.position = CGPointMake(self.frame.size.width + flappy.size.width / 2, randY);
-        flappy.hidden = NO;
+        [asteroid removeAllActions];
+        asteroid.position = CGPointMake(self.frame.size.width + asteroid.size.width / 2, randY);
+        asteroid.hidden = NO;
         
         CGPoint location = CGPointMake(-600, randY);
         
         SKAction *moveAction = [SKAction moveTo:location duration:randDuration];
         SKAction *doneAction = [SKAction runBlock:^{
-            flappy.hidden = YES;
+            asteroid.hidden = YES;
         }];
         
         SKAction *moveFlappyActionWithDone = [SKAction sequence:@[moveAction, doneAction]];
         
-        [flappy runAction:moveFlappyActionWithDone];
+        [asteroid runAction:moveFlappyActionWithDone];
     }
     
-//    for (SKSpriteNode *flappy in _flappyArray) {
-//        if (flappy.hidden) {
-//            continue;
-//        }
-//        for (SKSpriteNode *shipLaser in _shipLasers) {
-//            if (shipLaser.hidden) {
-//                continue;
-//            }
-//            
-//            if ([shipLaser intersectsNode:flappy]) {
-//                shipLaser.hidden = YES;
-//                flappy.hidden = YES;
-//                
-//                NSLog(@"you just destroyed a postmodern masterpiece");
-//                continue;
-//            }
-//        }
-//        if ([self.ship intersectsNode:flappy]) {
-//            flappy.hidden = YES;
-//            SKAction *blink = [SKAction sequence:@[[SKAction fadeOutWithDuration:0.1], [SKAction fadeInWithDuration:0.1]]];
-//            SKAction *blinkForTime = [SKAction repeatAction:blink count:4];
-//            [self.ship runAction:blinkForTime];
-//            _lives--;
-//            NSLog(@"your face feels alienated");
-//        }
-//    }
+    for (SKSpriteNode *asteroid in self.asteroidArray) {
+        if (asteroid.hidden) {
+            continue;
+        }
+        for (SKSpriteNode *shipLaser in _shipLasers) {
+            if (shipLaser.hidden) {
+                continue;
+            }
+            
+            if ([shipLaser intersectsNode:asteroid]) {
+                shipLaser.hidden = YES;
+                asteroid.hidden = YES;
+                continue;
+            }
+        }
+        
+        if ([self.ship intersectsNode:asteroid]) {
+            asteroid.hidden = YES;
+            SKAction *blink = [SKAction sequence:@[[SKAction fadeOutWithDuration:0.1], [SKAction fadeInWithDuration:0.1]]];
+            SKAction *blinkForTime = [SKAction repeatAction:blink count:4];
+            [self.ship runAction:blinkForTime];
+            _lives--;
+            NSLog(@"your face feels alienated");
+        }
+    }
     
     if (_lives <= 0) {
         NSLog(@"you lose :(");
@@ -245,22 +292,28 @@ typedef enum {
         NSLog(@"you win!");
         [self endTheScene:kEndReasonWin];
     }
+}
+
+- (void)moveShipWithTranslation:(CGPoint)translation {
+    // Animate the ship up or down based on finger position.
+    [self.ship runAction:[SKAction sequence:@[[SKAction moveByX:0 y:translation.y duration:0.4f]]]];
     
-    
-//    for (SKSpriteNode *flappy in self.flappyArray) {
-//        if ([self.mainCharacter intersectsNode:flappy]) {
-//            
-//            
-//            [self.mainCharacter removeFromParent];
-//            
-//            NSString *explosionPath = [[NSBundle mainBundle] pathForResource:@"Explosion" ofType:@"sks"];
-//            SKEmitterNode *burstNode = [NSKeyedUnarchiver unarchiveObjectWithFile:explosionPath];
-//            
-//            burstNode.position = self.mainCharacter.position;
-//            [self addChild:burstNode];
-//        }
-//    }
-    
+    // Change the animation based on up or down movement.
+    if (translation.y >= 0) {
+        NSLog(@"moving up");
+        [self.ship removeActionForKey:@"animatingShipCenter"];
+        [self.ship runAction:[SKAction repeatActionForever:[SKAction animateWithTextures:self.shipFramesUp
+                                                                            timePerFrame:0.1f
+                                                                                  resize:NO
+                                                                                 restore:YES]] withKey:@"animatingShipUp"];
+    } else if (translation.y < 0) {
+        NSLog(@"moving down");
+        [self.ship removeActionForKey:@"animatingShipCenter"];
+        [self.ship runAction:[SKAction repeatActionForever:[SKAction animateWithTextures:self.shipFramesDown
+                                                                            timePerFrame:0.1f
+                                                                                  resize:NO
+                                                                                 restore:YES]] withKey:@"animatingShipDown"];
+    }
 }
 
 - (void)endTheScene:(EndReason)endReason {
@@ -302,6 +355,24 @@ typedef enum {
     [restartLabel runAction:labelScaleAction];
     [label runAction:labelScaleAction];
     
+}
+
+- (void)startBackgroundMusic {
+    NSError *error;
+    NSURL *file = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"spaceTheme" ofType:@"mp3"]];
+    
+    backgroundAudioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:file error:&error];
+    if (error) {
+        NSLog(@"error in audio player: %@", error.localizedDescription);
+        return;
+    }
+    
+    [backgroundAudioPlayer prepareToPlay];
+    
+    // This will play the music infinitely
+    backgroundAudioPlayer.numberOfLoops = -1;
+    [backgroundAudioPlayer setVolume:1.0];
+    [backgroundAudioPlayer play];
 }
 
 -(float)randomValueBetween:(float)low andValue:(float)high
